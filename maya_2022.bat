85318ade849f583b7a90766f73b37263a6255fb7
aizwellenstan <aizwellenstan@gmail.com>
2023-05-22 18:42:02 +0800

    Update maya_2022.bat

--- maya_2022.bat
+++ maya_2022.bat
@@ -15,9 +15,9 @@ set SCRIPT_FOLDER=%MAYA_APP_DIR%\%MAYA_VERSION%\scripts
 if not exist %SCRIPT_FOLDER% mkdir %SCRIPT_FOLDER%
 
 robocopy I:\script\bin\td\bin\project\%PROJNAME%\mod %MODULE_FOLDER%\ /it /E /IS /IT
-robocopy I:\script\bin\td\bin\project\add\userSetup %SCRIPT_FOLDER%\ /it /E /IS /IT
+robocopy I:\script\bin\td\bin\project\%PROJNAME%\userSetup %SCRIPT_FOLDER%\ /it /E /IS /IT
 set MAYA_SHELF_PATH=J:/%PROJNAME%/work/project_tools/shelves
 
-set OCIO=J:\%PROJNAME%\proj\add\tool\settings\ocio\v5\config.ocio
+set OCIO=J:\%PROJNAME%\proj\%PROJNAME%\tool\settings\ocio\v5\config.ocio
 
 call %NMA_MAYA_EXE% %* -pythonver 2
